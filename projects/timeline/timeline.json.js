e94063215e0b8b6d0caad2fc7ce66484fbc487cb
jrrefugia <jrrefugia@gmail.com>
2017-09-25 15:57:06 -0400

    adding timeline

---
+++ projects/timeline/timeline.json.js
@@ -0,0 +1,909 @@
+var timeline = {
+	"timeline1": [{
+			"date": "20121001",
+			"heading": "October 2012",
+			"text": "Occupational Requirements Survey established as a test survey"
+		}, {
+			"date": "20121001",
+			"heading": "October 2012",
+			"text": "Feasibility testing in three phases"
+		}, {
+			"date": "20121101",
+			"heading": "November 2012",
+			"text": "Phase 1 test: Initial proof of concept"
+		}, {
+			"date": "20130101",
+			"heading": "January 2013",
+			"text": "Phase 2 test: Collection protocol testing"
+		}, {
+			"date": "20130401",
+			"heading": "April 2013",
+			"text": "Phase 3 test: Broad scale testing"
+		}, {
+			"date": "20131001",
+			"heading": "October 2013",
+			"text": "Six feasibility tests"
+		}, {
+			"date": "20131101",
+			"heading": "November 2013",
+			"text": "Only Efficiency innovations test"
+		}, {
+			"date": "20131201",
+			"heading": "December 2013",
+			"text": "Joint collection test "
+		}, {
+			"date": "20131201",
+			"heading": "December 2013",
+			"text": "New data element tests"
+		}, {
+			"date": "20131101",
+			"heading": "November 2013",
+			"text": "Central office collection test"
+		}, {
+			"date": "20140201",
+			"heading": "February 2014",
+			"text": "Alternative modes test"
+		}, {
+			"date": "20131101",
+			"heading": "November 2013",
+			"text": "Job Observations Test"
+		}, {
+			"date": "20141001",
+			"heading": "October 2014",
+			"text": "Pre-production testing"
+		}, {
+			"date": "20140901",
+			"heading": "September 2015 – December 2016",
+			"text": "First year of production data collection and estimation"
+		}, {
+			"date": "20151201",
+			"heading": "December 2016",
+			"text": "Published estimates from first production sample"
+		},
+		{
+			"date": "20120101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20121001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20121101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20121201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20131001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20131101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20131201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20141001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20141101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20141201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151201",
+			"heading": "test",
+			"text": "test"
+		}
+	],
+	"timeline2": [{
+			"date": "20150101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151201",
+			"heading": "test",
+			"text": "test"
+		}
+	],
+	"timeline3": [{
+			"date": "20150101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20160901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20161001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20161101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20161201",
+			"heading": "test",
+			"text": "test"
+		},
+	],
+	"timeline4": [{
+			"date": "20121001",
+			"heading": "October 2012",
+			"text": "Occupational Requirements Survey established as a test survey"
+		}, {
+			"date": "20121001",
+			"heading": "October 2012",
+			"text": "Feasibility testing in three phases"
+		}, {
+			"date": "20121101",
+			"heading": "November 2012",
+			"text": "Phase 1 test: Initial proof of concept "
+		}, {
+			"date": "20130101",
+			"heading": "January 2013",
+			"text": "Phase 2 test: Collection protocol testing "
+		}, {
+			"date": "20130401",
+			"heading": "April 2013",
+			"text": "Phase 3 test: Broad scale testing"
+		}, {
+			"date": "20131001",
+			"heading": "October 2013",
+			"text": "Six feasibility tests"
+		}, {
+			"date": "20131101",
+			"heading": "November 2013",
+			"text": "ORS Only Efficiency innovations test"
+		}, {
+			"date": "20131201",
+			"heading": "December 2013",
+			"text": "NCS/ORS Joint collection test "
+		}, {
+			"date": "20131201",
+			"heading": "December 2013",
+			"text": "New data element tests"
+		}, {
+			"date": "20131101",
+			"heading": "November 2013",
+			"text": "Central office collection test "
+		}, {
+			"date": "20140201",
+			"heading": "February 2014",
+			"text": "Alternative modes test "
+		}, {
+			"date": "20131101",
+			"heading": "November 2013",
+			"text": "Job Observations Test"
+		}, {
+			"date": "20141001",
+			"heading": "October 2014",
+			"text": "Pre-production testing"
+		}, {
+			"date": "20140901",
+			"heading": "September 2015 – December 2016",
+			"text": "First year of production data collection and estimation"
+		}, {
+			"date": "20191201",
+			"heading": "December 2016",
+			"text": "Published estimates from first production sample"
+		},
+		{
+			"date": "20120101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20120901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20121001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20121101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20121201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20130901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20131001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20131101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20131201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20140901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20141001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20141101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20141201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150201",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150301",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150401",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150501",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150601",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150701",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150801",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20150901",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151001",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151101",
+			"heading": "test",
+			"text": "test"
+		},
+		{
+			"date": "20151201",
+			"heading": "test",
+			"text": "test"
+		}
+	],
+	"timeline5": [{
+		"date": "20121001",
+		"heading": "October 2012",
+		"text": "Occupational Requirements Survey established as a test survey"
+	}, {
+		"date": "20121001",
+		"heading": "October 2012",
+		"text": "Feasibility testing in three phases"
+	}, {
+		"date": "20121101",
+		"heading": "November 2012",
+		"text": "Phase 1 test: Initial proof of concept "
+	}, {
+		"date": "20130101",
+		"heading": "January 2013",
+		"text": "Phase 2 test: Collection protocol testing "
+	}, {
+		"date": "20130401",
+		"heading": "April 2013",
+		"text": "Phase 3 test: Broad scale testing"
+	}, {
+		"date": "20131001",
+		"heading": "October 2013",
+		"text": "Six feasibility tests"
+	}, {
+		"date": "20131101",
+		"heading": "November 2013",
+		"text": "ORS Only Efficiency innovations test"
+	}, {
+		"date": "20131201",
+		"heading": "December 2013",
+		"text": "NCS/ORS Joint collection test "
+	}, {
+		"date": "20131201",
+		"heading": "December 2013",
+		"text": "New data element tests"
+	}, {
+		"date": "20131101",
+		"heading": "November 2013",
+		"text": "Central office collection test "
+	}, {
+		"date": "20140201",
+		"heading": "February 2014",
+		"text": "Alternative modes test "
+	}, {
+		"date": "20131101",
+		"heading": "November 2013",
+		"text": "Job Observations Test"
+	}, {
+		"date": "20141001",
+		"heading": "October 2014",
+		"text": "Pre-production testing"
+	}, {
+		"date": "20150901",
+		"heading": "September 2015 – December 2016",
+		"text": "First year of production data collection and estimation"
+	}, {
+		"date": "20161201",
+		"heading": "December 2016",
+		"text": "Published estimates from first production sample"
+	}],
+	"timeline6": [{
+		"date": "20121001",
+		"heading": "October 2012",
+		"text": "Occupational Requirements Survey established as a test survey"
+	}, {
+		"date": "20121001",
+		"heading": "October 2012",
+		"text": "Feasibility testing in three phases"
+	}, {
+		"date": "20121101",
+		"heading": "November 2012",
+		"text": "Phase 1 test: Initial proof of concept "
+	}, {
+		"date": "20130101",
+		"heading": "January 2013",
+		"text": "Phase 2 test: Collection protocol testing "
+	}, {
+		"date": "20130401",
+		"heading": "April 2013",
+		"text": "Phase 3 test: Broad scale testing"
+	}, {
+		"date": "20131001",
+		"heading": "October 2013",
+		"text": "Six feasibility tests"
+	}, {
+		"date": "20131101",
+		"heading": "November 2013",
+		"text": "ORS Only Efficiency innovations test"
+	}, {
+		"date": "20131201",
+		"heading": "December 2013",
+		"text": "NCS/ORS Joint collection test "
+	}, {
+		"date": "20131201",
+		"heading": "December 2013",
+		"text": "New data element tests"
+	}, {
+		"date": "20131101",
+		"heading": "November 2013",
+		"text": "Central office collection test "
+	}, {
+		"date": "20140201",
+		"heading": "February 2014",
+		"text": "Alternative modes test "
+	}, {
+		"date": "20131101",
+		"heading": "November 2013",
+		"text": "Job Observations Test"
+	}, {
+		"date": "20141001",
+		"heading": "October 2014",
+		"text": "Pre-production testing"
+	}, {
+		"date": "20150901",
+		"heading": "September 2015 – December 2016",
+		"text": "First year of production data collection and estimation"
+	}],
+	"timeline5-config": {
+		"monthlySegmentation": true
+	}
+};
