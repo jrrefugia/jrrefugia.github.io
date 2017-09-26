c931b2d2ea3e00b54107c89a0b6ad847e4417a33
jrrefugia <jrrefugia@gmail.com>
2017-09-26 16:26:07 -0400

    refactored noderest

--- projects/angular/nodeREST/app.js
+++ projects/angular/nodeREST/app.js
@@ -19,19 +19,108 @@ app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $url
 
 /*factories*/
 app.factory('customersAPI', ["$http",function($http){
+	function findCustomerIndex(id){
+		for(index in fakeDataArray){
+			if(fakeDataArray[index]._id == id){
+				return index;
+				break;
+			} 
+		}
+	}
+	var fakeDataArray = [{
+	    "_id": "59c6e6f5550beb0233722506",
+	    "name": "jerie",
+	    "purchases": 14,
+	    "email": "jerie@gmail.com"
+	}, {
+	    "_id": "59c6fc4bb70a5224308fcf64",
+	    "name": "christa",
+	    "purchases": 13,
+	    "email": "christa@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c74cc62f884611f0ef562a",
+	    "name": "bunky",
+	    "purchases": 516,
+	    "email": "bunky@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c750efea84112b0073d8fd",
+	    "name": "chris",
+	    "purchases": 345,
+	    "email": "chris@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c7512dea84112b0073d8ff",
+	    "name": "lynn",
+	    "purchases": 44,
+	    "email": "lynn@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c758c107b9d02c949e4d45",
+	    "name": "joe",
+	    "purchases": 252,
+	    "email": "joe@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c75f9f29aedc1de07ed79d",
+	    "name": "amy",
+	    "purchases": 15,
+	    "email": "amy@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c76255e5b54e2c243d5971",
+	    "name": "john",
+	    "purchases": 2345,
+	    "email": "john@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c766397ae25e27e09b53b9",
+	    "name": "mercy",
+	    "purchases": 23,
+	    "email": "mercy@gmail.com",
+	    "__v": 0
+	}, {
+	    "_id": "59c767267ae25e27e09b53ba",
+	    "name": "rob",
+	    "purchases": 324,
+	    "email": "rob@gmail.com",
+	    "__v": 0
+	}];
+	
 	return {
 		getData: function(id){
 			id = typeof id != "undefined" ? id : "";
-			return $http.get("/api/customers/"+id);
+			if(id == ""){
+				return fakeDataArray;
+			}else{
+				return fakeDataArray[findCustomerIndex(id)];
+			}
+			//return $http.get("/api/customers/"+id);
 		},
 		addData: function(data){
-			return $http.post("/api/customers/", data);
+			var d = new Date();
+			var n = d.getTime();
+			var object = {
+				_id: String(n),
+				__v: 0
+			}
+			for(key in data){
+				object[key] = key == "purchases" ? Number(data[key]) : data[key];				
+			}
+			fakeDataArray.push(object);
+			
+			//return $http.post("/api/customers/", data);
 		},
 		updateData: function(id, data){
-			return $http.put("/api/customers/" + id, data);
+			for(key in data){
+				fakeDataArray[findCustomerIndex(id)][key] = data[key];
+			}
+			//return $http.put("/api/customers/" + id, data);
 		},
 		deleteData: function(id){
-			return $http.delete("/api/customers/" + id);
+			fakeDataArray.splice(findCustomerIndex(id), 1);
+			//return $http.delete("/api/customers/" + id);
 		}
     };
 }]);
@@ -41,10 +130,8 @@ app.controller('customersCtrl', ['$scope', '$location','$stateParams','customers
 	$scope.params = {
 		name:"",
 		purchases:"",
-		url:""
+		email:""
 	}
-	$scope.fakeArray = [{"_id":"59c6e6f5550beb0233722506","name":"joe","purchases":14,"url":"dsfdsf"},{"_id":"59c6fc4bb70a5224308fcf64","name":"jerie","purchases":13,"url":"sdf","__v":0},{"_id":"59c74cc62f884611f0ef562a","name":"jerie","purchases":516,"url":"google.com","__v":0},{"_id":"59c750efea84112b0073d8fd","name":"jdklfj","purchases":345,"url":"dfs","__v":0},{"_id":"59c7512dea84112b0073d8ff","name":"adsf","purchases":44,"url":"adsf","__v":0},{"_id":"59c758c107b9d02c949e4d45","name":"joe","purchases":252,"url":"shmo","__v":0},{"_id":"59c75f9f29aedc1de07ed79d","name":"killa","purchases":15,"url":"killa","__v":0},{"_id":"59c76255e5b54e2c243d5971","name":"fddsf","purchases":2347654454345355,"url":"asd","__v":0},{"_id":"59c766397ae25e27e09b53b9","name":"mercy","purchases":23,"url":"mercy.com","__v":0},{"_id":"59c767267ae25e27e09b53ba","name":"jerie","purchases":324,"url":"here","__v":0}];
-	
 
 	if($stateParams.id){
 		/*
@@ -52,28 +139,28 @@ app.controller('customersCtrl', ['$scope', '$location','$stateParams','customers
 			$scope.customer = response.data;
 			setScopeParams(response.data);
 		});
-		*/
-		for(index in $scope.fakeArray){
-			if($scope.fakeArray[index]._id == $stateParams.id){
-				$scope.customer = $scope.fakeArray[index];
-				setScopeParams($scope.customer);
-				break;
-			}
-		}
+		*/		
+		$scope.customer = customersAPI.getData($stateParams.id);
+		setScopeParams($scope.customer);
 	}else if($stateParams.id != ""){
 		/*
 		customersAPI.getData().then(function(response){
 			$scope.allCustomers = response.data;
-		});	
+		});
 		*/
-		$scope.allCustomers = $scope.fakeArray;
+		$scope.allCustomers = customersAPI.getData();
 	}
 	
 	$scope.addCustomer = function(){
 		if(!scopeParamsIsValid()){return;}
+		/*
 		customersAPI.addData($scope.params).then(function(){
 			$location.path('/home');
 		});
+		*/
+		customersAPI.addData($scope.params);
+		$location.path('/home');
+		
 	}
 	$scope.updateCustomer = function(customer){
 		if(!scopeParamsIsValid()){return;}
@@ -82,20 +169,17 @@ app.controller('customersCtrl', ['$scope', '$location','$stateParams','customers
 			$location.path('/home');
 		});
 		*/
-		
-		for(key in $scope.params){
-			var value = key == "purchases" ? Number($scope.params[key]) : $scope.params[key];
-			$scope.fakeArray[$scope.fakeArray.indexOf($scope.fakeArray[index])][key] = value;
-		}
+		customersAPI.updateData(customer._id, $scope.params);
 		$location.path('/home');
-		
-		
-		
 	}
 	$scope.deleteCustomer = function(customer){
+		/*
 		customersAPI.deleteData(customer._id).then(function(){
 			$location.path('/home');
 		});
+		*/
+		customersAPI.deleteData(customer._id);
+		$location.path('/home');
 	}
 	
 	$scope.incrementPurchases = function(customer){
