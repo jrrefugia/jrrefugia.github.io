e018937e03ed1fb9523dd4283896b68d7283845c
jrrefugia <jrrefugia@gmail.com>
2019-07-15 01:00:58 -0400

    house of reps project
    house of reps project

---
+++ projects/react/hor/precache-manifest.aa3c0bbe9db764e2e651dd952d3f7d71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c92d059c1574177afa595a57a47f9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5eacdb80ceb5d374fa8",
+    "url": "/static/css/main.a9c39b48.chunk.css"
+  },
+  {
+    "revision": "9df4ea570e5096125f56",
+    "url": "/static/js/2.2c343630.chunk.js"
+  },
+  {
+    "revision": "c5eacdb80ceb5d374fa8",
+    "url": "/static/js/main.07e6faa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
