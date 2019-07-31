33afc3366a29f782b626630418d09ecbe1a8793d
jrrefugia <jrrefugia@gmail.com>
2019-07-31 18:42:43 -0400

    reuploading

---
+++ projects/react/hor/precache-manifest.49293eedd35992fded78d02f488ca4e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442cac0cb052b75c5a227dc81ce0594a",
+    "url": "/projects/react/hor/index.html"
+  },
+  {
+    "revision": "2bb1c0b9fa203a6224c7",
+    "url": "/projects/react/hor/static/css/main.1458ab54.chunk.css"
+  },
+  {
+    "revision": "542cc29c231d0412048b",
+    "url": "/projects/react/hor/static/js/2.8e19009a.chunk.js"
+  },
+  {
+    "revision": "2bb1c0b9fa203a6224c7",
+    "url": "/projects/react/hor/static/js/main.2a84f539.chunk.js"
+  },
+  {
+    "revision": "9fb5390cc5b72e6c836f",
+    "url": "/projects/react/hor/static/js/runtime~main.da66b862.js"
+  }
+]);
